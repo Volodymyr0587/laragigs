576fb21fc187b56d4086881df7b759d76e1a7aa4
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-06-16 12:38:40 +0300

    05_06_Routing & Responses; Wildcard Endpoints

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function() {
+    return response('<h1>Hello From Laravel! )))</h1>', 200)
+        ->header('Content-Type', 'text/plain')
+        ->header('foo', 'bar');
+});
+
+Route::get('/posts/{id}', function($id) {
+    return response('Post ' . $id);
+});
