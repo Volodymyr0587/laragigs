e3e5a2de79b39033daf00cefb5b34bff72a0a81e
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-06-17 12:59:21 +0300

    07 - 07 - Route Constraints (Обмеження маршруту) - fix git push

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::get('/hello', function() {
         ->header('foo', 'bar');
 });
 
+Route::get('/posts/{id}', function($id) {
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
