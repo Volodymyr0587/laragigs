82a835accd370c02daa01441e26153f323b53f07
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-06-26 11:03:11 +0300

    35 - Create Listing Form

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,8 @@ use Illuminate\Support\Facades\Route;
 // All Listings
 Route::get('/', [ListingController::class, 'index']);
 
+// Show Create Form
+Route::get('listings/create', [ListingController::class, 'create']);
+
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
