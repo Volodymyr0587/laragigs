76d102f6e68412c3f9262054dfbb999c315ea491
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-07-01 11:27:21 +0300

    50 - Relationships

--- database/migrations/2023_06_20_095347_create_listings_table.php
+++ database/migrations/2023_06_20_095347_create_listings_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); //If you delete a user, all records associated with this particular user are deleted.
             $table->string('title');
             $table->string('logo')->nullable();
             $table->string('tags');
